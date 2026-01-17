abb6a1597d1590a05828f429edce457c5b9224e3
Toyakki <totto1001@icloud.com>
2026-01-16 22:01:49 -0500

    pipeline docstring added.

--- agents/agents.py
+++ agents/agents.py
@@ -332,6 +332,20 @@ def create_graph():
     return graph
 
 def run_pipeline(user_input : dict) -> dict[Beat, list[QuestionObject]]:
+    """
+    Exapmle of an user input:
+    exp1 = {
+    "scholarship_name": "Vector scholarships",
+    "program_type": "Community Leadership",
+    "goal_one_liner": "Machine Learning Workshop hosts for academic engagement.",
+    "resume_points": [
+        "Led a team of 5 in developing a 3D CNN to decode emotional state from 7tfMRI brain images, improved the test accuracy to 80%."
+        "Organized and hosted weekly study paper reading groups for over 15 students in transformers.",
+        "Conducted research under Prof Geoffery Hinton, resulting in a published paper in a Neurlps 2025 conference.",
+        ],
+    }
+    """
+    
     try:
         user_input = UserInput.model_validate(exp1)
         graph = create_graph()
