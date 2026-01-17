f478389af5cfc58dcfc393e46b918017e8c3220e
AgricultureAlex <alexma000003@gmail.com>
2026-01-16 23:31:40 -0500

    added more .env loading, still debugging test.py on second system

--- econf/env.py
+++ econf/env.py
@@ -3,24 +3,36 @@ from os import environ
 from os.path import exists
 
 if not exists(".env"):
-	raise Exception(".env file not found! Please create one using .env.example as a reference!")
+    raise Exception(
+        ".env file not found! Please create one using .env.example as a reference!"
+    )
 
 if not load_dotenv(dotenv_path=".env"):
-	raise Exception("failed to parse .env file! Please fill it out using .env.example as a reference!")
+    raise Exception(
+        "failed to parse .env file! Please fill it out using .env.example as a reference!"
+    )
+
 
 def get_env(var: str) -> str:
-	if var in environ:
-		return environ[var]
-	raise Exception(f"WARNING: failed to parse env var '{var}' from .env: did you create and fill it out?")
+    load_dotenv()
+    if var in environ:
+        return environ[var]
+    raise Exception(
+        f"WARNING: failed to parse env var '{var}' from .env: did you create and fill it out?"
+    )
+
 
 with open(".env.example", "r") as envex:
-	data = envex.read()
+    data = envex.read()
 
 lines = [i for i in data.split("\n") if i]
 for line in lines:
-	var = line.split("=")[0]
-	if not get_env(var):
-		raise Exception(f"WARNING: failed to parse env var '{var}' from .env: did you create and fill it out?")
+    var = line.split("=")[0]
+    if not get_env(var):
+        raise Exception(
+            f"WARNING: failed to parse env var '{var}' from .env: did you create and fill it out?"
+        )
+
 
 def _set_env(key: str) -> None:
     environ[var] = get_env(key)
